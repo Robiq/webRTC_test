0a2fe22c0fb32f79d907348d86b552c13c10a45d
Robin <robin@wub.no>
2018-04-13 14:52:37 +0900

    Ready for test. Likely functional

--- Reverse_case/Client/client.js
+++ Reverse_case/Client/client.js
@@ -3,7 +3,6 @@ var uuid=-1;
 var test = 1;
 var log='';
 var serverConnection;
-var keep;
 
 var peerConnectionConfig = {'iceServers': [{'url': 'stun:stun.gmx.net'}]};
 
@@ -24,77 +23,106 @@ function start() {
     errorHandler("Running test: " + test);
     peerConnection = new RTCPeerConnection(peerConnectionConfig);
     peerConnection.onicecandidate = gotIceCandidate;
+    peerConnection.oniceconnectionstatechange = iceChange;
+    //datachannel
+    peerConnection.createDataChannel('test', {reliable: true})
+    //Creates offer
+    peerConnection.createOffer().then(function (description){
+        errorHandler('got description(webRTC): ', description);
+        peerConnection.setLocalDescription(description).catch(errorHandler);
+    }).catch(errorHandler);
 }
 
 function gotMessageFromServer(message) {
-    if(!peerConnection){
-        start();
-    } 
-        
 
     var signal = JSON.parse(message.data);
+    console.log(signal);
+    var signal2 = JSON.parse(message);
+    console.log(signal2);
 
-    // Ignore messages from ourself
-    if(signal.uuid == uuid){
-        errorHandler('KeepAlive received');
+    switch(signal){
+    	//Ignore messages from ourself
+    	case uuid:
+    		break;
+    	//Set own uuid and start
+    	case set:
+	        uuid = signal.uuid
+	        document.getElementById("ws").className = '';
+	        //Start WebRTC!
+	        start();      
+    		break;
+    	//Receive Answer from server
+    	case sdp:
+    		handleSDP(signal); break;
+       //Ready for next test
+    	case reset:
+    	    test++;
+    		start();
+    		break;
+    	default: errorHandler("Unknown signal!", signal);
     }
+}
+function handleSDP(signal){
+	//Simulates client-delay
+    if( test < 6){
+        setDescription(signal.sdp);
+    }else{
+        //Delay currently 5sec - 5 min
+        errorHandler('Delaying Client for case: ' + test);
+        switch(test){
+            case 6:
+            case 11: setTimeout(setDescription, 5000, signal.sdp); break;
+            case 7: 
+            case 12: setTimeout(setDescription, 30000, signal.sdp); break;
+            case 8: 
+            case 13: setTimeout(setDescription, 60000, signal.sdp); break;
+            case 9: 
+            case 14: setTimeout(setDescription, 120000, signal.sdp); break;
+            case 10: 
+            case 15: setTimeout(setDescription, 300000, signal.sdp); break;
+            default: errorHandler("Testcase not recognized - Client delay");
+        }
+    }
+}
 
-    if(signal.set){
-        uuid = signal.uuid
-        document.getElementById("ws").className = '';
-        //keep=setInterval(keepAlive, 75000);
-    }
-    //Receive offer from server
-    if(signal.sdp) {
-        //Simulates server-delay
-        if( test > 5 && test < 11 ){
-            setDescription(signal.sdp);
-        }else{
+function gotIceCandidate(event) {
+    if(event.candidate == null){
+        errorHandler('ICE done. Offer: ', peerConnection.localDescription);
+        //SENDS ANSWER
+        //Simulates Server-delay
+        if( test < 6  && test > 10 ){
             //Delay currently 5sec - 5 min
-            //TODO MOVE TO ICEGATHERING FINISHED! (Server case!)
-            //JUST swap places and let client create offer and server receive and we gud?
-            errorHandler('Delaying server for case: ' + test);
+            errorHandler('Delaying Server for case: ' + test);
             switch(test){
                 case 1:
-                case 11: setTimeout(setDescription, 5000, signal.sdp); break;
+                case 11: setTimeout(sendSDP, 5000); break;
                 case 2: 
-                case 12: setTimeout(setDescription, 30000, signal.sdp); break;
+                case 12: setTimeout(sendSDP, 30000); break;
                 case 3: 
-                case 13: setTimeout(setDescription, 60000, signal.sdp); break;
+                case 13: setTimeout(sendSDP, 60000); break;
                 case 4: 
-                case 14: setTimeout(setDescription, 120000, signal.sdp); break;
+                case 14: setTimeout(sendSDP, 120000); break;
                 case 5: 
-                case 15: setTimeout(setDescription, 300000, signal.sdp); break;
-                default: errorHandler("Testcase not recognized - Server delay");
+                case 15: setTimeout(sendSDP, 300000); break;
+                default: errorHandler("Testcase not recognized - Server Delay");
             }
-        }
-    } else if(signal.reset){
-        if(signal.success){
-            errorHandler('Test '+ test + ' succeeded');
-            updateHTML(true);
         }else{
-            errorHandler('Test '+ test + ' failed');       
-            updateHTML(false);
+            sendSDP();
         }
-
-        //Removed one updateHTML from each if-clause
-        //Handling new tests/finished testing
-        if(++test == 6){
-            document.getElementById("T1D").className = '';
-            document.getElementById("T2").className = '';
-            serverConnection.send(JSON.stringify({'log': log, 'uuid': uuid}));
-        }else if(test == 11){
-            document.getElementById("T2D").className = '';
-            document.getElementById("T3").className = '';
-            serverConnection.send(JSON.stringify({'log': log, 'uuid': uuid}));
-        }else if(test>15){
-            document.getElementById("T3D").className = '';
-            errorHandler('Testing finished!');
-            serverConnection.send(JSON.stringify({'log': log, 'uuid': uuid}));
-            //clearInterval(keep);
-        }
-        start();
     }
+}
+
+function iceChange(event){
+    try{
+        var state = peerConnection[curUUID].iceConnectionState;
+        errorHandler("New ICE state: ", state);
+    }catch(e){errorHandler('iceChange: ', e);}
+    /*if(state == 'connected'){
+        var dt = new Date();
+        dt = dt - prevDt;
+        let sec = Math.floor(dt/1000);
+        errorHandler('It took ' + sec + ' sec to reach connected state.');        
+    }*/
 }
 
 function setDescription(sig){
@@ -102,7 +130,14 @@ function setDescription(sig){
         // Only create answers in response to offers
         if(sig.type == 'offer') {
             //SENDS ANSWER
+            console.warn("FUCK! WRONG!! - Received an offer!");
             peerConnection.createAnswer().then(createdDescription).catch(errorHandler);
+        }else{
+            console.log("Received answer!", sig);
+            //Handle testing of connection!
+            //Need short wait here to allow ICE-candidates to finish negotiation!
+			//Try with 0.5sec delay!
+			setTimeout(runTest, 500);
         }
     }).catch(errorHandler);
 }
@@ -116,49 +151,33 @@ function createdDescription(description) {
     }).catch(errorHandler);
 }
 
-function gotIceCandidate(event) {
-    if(event.candidate == null){
-        errorHandler('ICE done. Answer: ', peerConnection.localDescription);
-        //SENDS ANSWER
-        //Simulates client-delay
-        if( test > 5 ){
-            //Delay currently 5sec - 5 min
-            errorHandler('Delaying client for case: ' + test);
-            switch(test){
-                case 6:
-                case 11: setTimeout(sendSDP, 5000); break;
-                case 7: 
-                case 12: setTimeout(sendSDP, 30000); break;
-                case 8: 
-                case 13: setTimeout(sendSDP, 60000); break;
-                case 9: 
-                case 14: setTimeout(sendSDP, 120000); break;
-                case 10: 
-                case 15: setTimeout(sendSDP, 300000); break;
-                default: errorHandler("Testcase not recognized - Client Delay");
-            }
-        }else{
-            sendSDP();
-        }
-    }
-}
-
 function sendSDP(){
-    errorHandler('Sending reply');
+    errorHandler('Sending offer');
     serverConnection.send(JSON.stringify({'sdp': peerConnection.localDescription, 'uuid': uuid}));
 }
 
-function errorHandler(error, obj=null) {
-    var dt = new Date();
-    var utcDate = dt.toUTCString();
-    if(obj){
-        obj=JSON.stringify(obj);
-        log += "["+utcDate + "]:\n " + error + obj + '\n';
-        console.log(error + obj);
+//Runs the current test
+function runTest(){
+    var res = 0;
+    //Read connection state
+    var state = peerConnection.iceConnectionState;
+    //Log it
+    errorHandler('State of connection: ', state);
+    //Test connection state
+    if(state == 'connected' || state=="completed"){
+        //Connected means goodie!
+        errorHandler('Test ' + test + ' succeeded!');
+        res = true;
     }else{
-        log += "["+utcDate + "]:\n " + error+'\n';
-        console.log(error);
+        errorHandler('Test ' + test + ' failed!');
+        var dt = new Date();
+        var utcDate = "[" + dt.toLocaleDateString() + " | " + dt.toLocaleTimeString() + "]";
+        res = false;
     }
+    peerConnection=null;
+    //Send result to Server (acting as client)
+    conn[curUUID].send(JSON.stringify({'reset': true, 'success': res}));
+    updateHTML(res);
 }
 
 //Updates html
@@ -176,11 +195,15 @@ function updateHTML(res){
     }
 }
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function errorHandler(error, obj=null) {
+    var dt = new Date();
+    var utcDate = dt.toUTCString();
+    if(obj){
+        obj=JSON.stringify(obj);
+        log += "["+utcDate + "]:\n " + error + obj + '\n';
+        console.log(error + obj);
+    }else{
+        log += "["+utcDate + "]:\n " + error+'\n';
+        console.log(error);
+    }
 }
-/*
-function keepAlive(ws){
-    errorHandler('keepAlive sent!');
-    serverConnection.send(JSON.stringify({'keepalive': uuid}));
-}*/
---
+++ Reverse_case/Client/client_old.js
@@ -0,0 +1,188 @@
+var peerConnection;
+var uuid=-1;
+var test = 1;
+var log='';
+var serverConnection;
+var keep;
+
+var peerConnectionConfig = {'iceServers': [{'url': 'stun:stun.gmx.net'}]};
+
+//Activates when page is loaded
+function pageReady() {
+
+    serverConnection = new WebSocket('ws://' + window.location.hostname + ':8443');
+    serverConnection.onmessage = gotMessageFromServer;
+
+    var constraints = {
+        video: false,
+        audio: false,
+    };
+}
+
+//Starts peer connection
+function start() {
+    errorHandler("Running test: " + test);
+    peerConnection = new RTCPeerConnection(peerConnectionConfig);
+    peerConnection.onicecandidate = gotIceCandidate;
+}
+
+function gotMessageFromServer(message) {
+    if(!peerConnection){
+        start();
+    } 
+        
+
+    var signal = JSON.parse(message.data);
+
+    // Ignore messages from ourself
+    if(signal.uuid == uuid){
+        errorHandler('KeepAlive received');
+    }
+
+    if(signal.set){
+        uuid = signal.uuid
+        document.getElementById("ws").className = '';
+        //keep=setInterval(keepAlive, 75000);
+    }
+    //Receive Answer from server
+    if(signal.sdp) {
+        //Simulates server-delay
+        if( test < 6){
+            setDescription(signal.sdp);
+        }else{
+            //Delay currently 5sec - 5 min
+            //TODO MOVE TO ICEGATHERING FINISHED! (Server case!)
+            //JUST swap places and let client create offer and server receive and we gud?
+            errorHandler('Delaying Client for case: ' + test);
+            switch(test){
+                case 6:
+                case 11: setTimeout(setDescription, 5000, signal.sdp); break;
+                case 7: 
+                case 12: setTimeout(setDescription, 30000, signal.sdp); break;
+                case 8: 
+                case 13: setTimeout(setDescription, 60000, signal.sdp); break;
+                case 9: 
+                case 14: setTimeout(setDescription, 120000, signal.sdp); break;
+                case 10: 
+                case 15: setTimeout(setDescription, 300000, signal.sdp); break;
+                default: errorHandler("Testcase not recognized - Client delay");
+            }
+        }
+    } else if(signal.reset){
+        if(signal.success){
+            errorHandler('Test '+ test + ' succeeded');
+            updateHTML(true);
+        }else{
+            errorHandler('Test '+ test + ' failed');       
+            updateHTML(false);
+        }
+
+        //Handling new tests/finished testing
+        if(++test == 6){
+            document.getElementById("T1D").className = '';
+            document.getElementById("T2").className = '';
+            serverConnection.send(JSON.stringify({'log': log, 'uuid': uuid}));
+        }else if(test == 11){
+            document.getElementById("T2D").className = '';
+            document.getElementById("T3").className = '';
+            serverConnection.send(JSON.stringify({'log': log, 'uuid': uuid}));
+        }else if(test>15){
+            document.getElementById("T3D").className = '';
+            errorHandler('Testing finished!');
+            serverConnection.send(JSON.stringify({'log': log, 'uuid': uuid}));
+            //clearInterval(keep);
+        }
+        start();
+    }
+}
+
+function setDescription(sig){
+    peerConnection.setRemoteDescription(new RTCSessionDescription(sig)).then(function() {
+        // Only create answers in response to offers
+        if(sig.type == 'offer') {
+            //SENDS ANSWER
+            console.warn("FUCK! WRONG!!");
+            peerConnection.createAnswer().then(createdDescription).catch(errorHandler);
+        }else{
+
+        }
+    }).catch(errorHandler);
+}
+
+//Answers the offer
+function createdDescription(description) {
+    errorHandler('Created description', description);
+
+    peerConnection.setLocalDescription(description).then(function() {
+        errorHandler('Local description set!');
+    }).catch(errorHandler);
+}
+
+function gotIceCandidate(event) {
+    if(event.candidate == null){
+        errorHandler('ICE done. Offer: ', peerConnection.localDescription);
+        //SENDS ANSWER
+        //Simulates Server-delay
+        if( test < 6  && test > 10 ){
+            //Delay currently 5sec - 5 min
+            errorHandler('Delaying Server for case: ' + test);
+            switch(test){
+                case 1:
+                case 11: setTimeout(sendSDP, 5000); break;
+                case 2: 
+                case 12: setTimeout(sendSDP, 30000); break;
+                case 3: 
+                case 13: setTimeout(sendSDP, 60000); break;
+                case 4: 
+                case 14: setTimeout(sendSDP, 120000); break;
+                case 5: 
+                case 15: setTimeout(sendSDP, 300000); break;
+                default: errorHandler("Testcase not recognized - Server Delay");
+            }
+        }else{
+            sendSDP();
+        }
+    }
+}
+
+function sendSDP(){
+    errorHandler('Sending reply');
+    serverConnection.send(JSON.stringify({'sdp': peerConnection.localDescription, 'uuid': uuid}));
+}
+
+function errorHandler(error, obj=null) {
+    var dt = new Date();
+    var utcDate = dt.toUTCString();
+    if(obj){
+        obj=JSON.stringify(obj);
+        log += "["+utcDate + "]:\n " + error + obj + '\n';
+        console.log(error + obj);
+    }else{
+        log += "["+utcDate + "]:\n " + error+'\n';
+        console.log(error);
+    }
+}
+
+//Updates html
+function updateHTML(res){
+    var out='';
+    if(res) out='DONE! Status: SUCESS';
+    else    out='DONE! Status: FAILED';
+    errorHandler('Displaying test ' + test + ' result =' + out);
+    if(test < 16){
+        let el = document.getElementById(test);
+        let x = el.innerHTML;
+        x = x.replace("#", out);
+        el.innerHTML=x;
+        el.className = '';
+    }
+}
+/*
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}*/
+/*
+function keepAlive(ws){
+    errorHandler('keepAlive sent!');
+    serverConnection.send(JSON.stringify({'keepalive': uuid}));
+}*/
